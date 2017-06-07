a2384297976163985662b15dbd1c26d2c94a8d76
Prathap <prathap.bezawada@gmail.com>
2017-06-07 23:16:22 +0530

    Added Responce Parser

--- app/src/main/java/com/mobileeftpos/android/eftpos/utils/JSONUtil.java
+++ app/src/main/java/com/mobileeftpos/android/eftpos/utils/JSONUtil.java
@@ -5,34 +5,13 @@ import android.util.Log;
 
 import com.google.gson.JsonParseException;
 import com.mobileeftpos.android.eftpos.model.AlipayResponceModel;
-import com.vgrowedunet.parentapp.pojo.AssignmentPOJO;
-import com.vgrowedunet.parentapp.pojo.AttendancePOJO;
-import com.vgrowedunet.parentapp.pojo.ExamsDatesPOJO;
-import com.vgrowedunet.parentapp.pojo.ExamsPOJO;
-import com.vgrowedunet.parentapp.pojo.FeeTransactions;
-import com.vgrowedunet.parentapp.pojo.LeaveApplicationPOJO;
-import com.vgrowedunet.parentapp.pojo.LibraryBooksFinePOJO;
-import com.vgrowedunet.parentapp.pojo.LibraryBooksReturnPOJO;
-import com.vgrowedunet.parentapp.pojo.LibraryBooksTakenPOJO;
-import com.vgrowedunet.parentapp.pojo.NotificationsPOJO;
-import com.vgrowedunet.parentapp.pojo.PerformanceDetails;
-import com.vgrowedunet.parentapp.pojo.PerformancePOJO;
-import com.vgrowedunet.parentapp.stationary.ViewOrderItemsPOJO;
-import com.vgrowedunet.parentapp.stationary.ViewOrdersPOJO;
-import com.vgrowedunet.parentapp.transportnew.EndPointPOJO;
-import com.vgrowedunet.parentapp.transportnew.RouteMap;
-import com.vgrowedunet.parentapp.transportnew.StartPointPojo;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
